3d7d2cf0762da78f7f9b3d0098572ed343b0365d
Andrea <a.pignieri@studenti.unina.it>
2026-01-18 15:44:54 +0100

    Added error handling and memory cleaning

--- server/clientHandler.c
+++ server/clientHandler.c
@@ -12,11 +12,22 @@
 
 #define TRUE 1
 
+void performCleanup(int socket, cJSON *req, cJSON *resp, char *respStr) {
+    if (respStr != NULL) free(respStr);
+    if (resp != NULL) cJSON_Delete(resp);
+    if (req != NULL) cJSON_Delete(req);
+    close(socket);
+}
+
 void *clientHandler(void* socketPtr) {
     int clientSocket = *((int*)socketPtr);
     free(socketPtr);
 
     char buf[1024];
+    cJSON *json = NULL;
+    cJSON *response = NULL;
+    char *responseStr = NULL;
+
     ssize_t n = recv(clientSocket, buf, sizeof(buf)-1, 0);
 
     if (n > 0)
@@ -25,52 +36,46 @@ void *clientHandler(void* socketPtr) {
     }
     else {
         errorHandler(clientSocket, DATA_RECEPTION_ERROR);
+        performCleanup(clientSocket, NULL, NULL, NULL);
         return NULL;
     }
 
-    cJSON *json = cJSON_Parse(buf);
+    json = cJSON_Parse(buf);
     if (json == NULL) {
         const char *error_ptr = cJSON_GetErrorPtr();
         if (error_ptr != NULL) {
             printf("Error: %s\n", error_ptr);
         }
-        cJSON_Delete(json);
         errorHandler(clientSocket, JSON_ERROR);
+        performCleanup(clientSocket, json, NULL, NULL);
         return NULL;
     }
     
-    cJSON *response = processRequest(json);
+    response = processRequest(json);
     if (response == NULL) {
-        cJSON_Delete(json);
         errorHandler(clientSocket, REQUEST_PROCESSING_ERROR);
+        performCleanup(clientSocket, json, NULL, NULL);
         return NULL;
     }
     
-    //TODO: FIX ERROR HANDLING
     cJSON *statusItem = cJSON_GetObjectItemCaseSensitive(response, "status");
     if (cJSON_IsString(statusItem) && strcmp(statusItem->valuestring, "error") == 0) {
         cJSON *errorMessageItem = cJSON_GetObjectItemCaseSensitive(response, "message");
         const char *errorMessage = cJSON_IsString(errorMessageItem) ? errorMessageItem->valuestring : "Unknown error";
-        cJSON_Delete(json);
         errorHandler(clientSocket, errorMessage);
-        cJSON_Delete(response);
+        performCleanup(clientSocket, json, response, NULL);
         return NULL;
     }
 
-    char *responseStr = cJSON_PrintUnformatted(response);
+    responseStr = cJSON_PrintUnformatted(response);
     if (responseStr == NULL) {
-        cJSON_Delete(response);
-        cJSON_Delete(json);
         errorHandler(clientSocket, RESPONSE_CREATION_ERROR);
+        performCleanup(clientSocket, json, response, NULL);
         return NULL;
     }
     send(clientSocket, responseStr, strlen(responseStr), 0);
 
-
-    free(responseStr);
-    cJSON_Delete(response);
-    cJSON_Delete(json);
-    close(clientSocket);
+    performCleanup(clientSocket, json, response, responseStr);
 
     return NULL;
 }
